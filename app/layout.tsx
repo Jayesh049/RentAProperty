e9ad2631e176128978e810a06fdb8710f868a4c9
Joksepha <jampotom421@gmail.com>
2023-06-17 17:19:45 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+
+import { Nunito } from "next/font/google";
+import Navbar from './components/navbar/navbar';
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font= Nunito({
+    subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        </body>
+    </html>
+  )
+}
