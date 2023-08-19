cbb227cbf68b9b3366d9e225a565c494d4026f0b
Jayesh049 <jampotom421@gmail.com>
2023-08-20 00:52:25 +0530

    added upto favorites

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,9 @@ export default async function RootLayout({
           <LoginModal /> 
           <Navbar currentUser = {currentUser}/>
         </ClientOnly>
+        <div className='pb-20 pt-28'>
         {children}
+        </div>
         </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,51 @@
-export default function Home() {
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/ListingCard";
+
+export default async function Home() {
+
+  const currentUser = await getCurrentUser();
+  const listings = await getListings();
+
+    if(listings.length === 0) {
+      return (
+        <ClientOnly>
+          <EmptyState showReset />
+        </ClientOnly>
+      )
+    }
   return (
-   <div className="text-rose-500 text-2xl">Hello Airbnb!</div>
+ 
+      <ClientOnly>
+        <Container>
+          <div className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl-grid-cols-6
+            gap-8
+          "
+          >
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard 
+                  key={listing.id}
+                  data={listing}
+                  currentUser={currentUser}
+                />
+                // <div>{listing.title}</div>
+              )
+            })}
+          </div>
+        </Container>
+      </ClientOnly>
   )
 }
